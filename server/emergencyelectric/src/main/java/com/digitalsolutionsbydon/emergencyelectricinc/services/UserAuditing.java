489a3bad2799e46846085be222ee55993c317c3c
Donald Whitely <dswhitely1@gmail.com>
2019-09-16 10:39:46 -0400

    Auditing Service Defined

---
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/UserAuditing.java
@@ -0,0 +1,30 @@
+package com.digitalsolutionsbydon.emergencyelectricinc.services;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserAuditing implements AuditorAware<String>
+{
+    @Override
+    public Optional<String> getCurrentAuditor()
+    {
+        String uName;
+        Authentication authentication = SecurityContextHolder.getContext()
+                                                             .getAuthentication();
+
+        if (authentication != null)
+        {
+            uName = authentication.getName();
+        } else
+        {
+            uName = "SYSTEM";
+        }
+
+        return Optional.of(uName);
+    }
+}
