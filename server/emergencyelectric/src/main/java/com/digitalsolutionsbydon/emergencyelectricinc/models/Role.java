677856038ea1713383515871025730422eb2a5db
Donald Whitely <dswhitely1@gmail.com>
2019-09-16 12:52:20 -0400

    Role Model Defined

---
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/models/Role.java
@@ -0,0 +1,61 @@
+package com.digitalsolutionsbydon.emergencyelectricinc.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Role extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("role")
+    private List<UserRoles> userRoles = new ArrayList<>();
+
+    public Role()
+    {
+    }
+
+    public Role(String name)
+    {
+        this.name = name;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public List<UserRoles> getUserRoles()
+    {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles)
+    {
+        this.userRoles = userRoles;
+    }
+}
