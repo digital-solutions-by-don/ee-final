bb0030df074d51d593c99ca619f2038f9890e3f2
Donald Whitely <dswhitely1@gmail.com>
2019-09-16 10:11:52 -0400

    Backend BoilerPlate

---
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/handlers/RestExceptionHandler.java
@@ -0,0 +1,147 @@
+package com.digitalsolutionsbydon.emergencyelectricinc.handlers;
+
+import com.digitalsolutionsbydon.emergencyelectricinc.exceptions.BadRequestException;
+import com.digitalsolutionsbydon.emergencyelectricinc.exceptions.NotAuthorizedException;
+import com.digitalsolutionsbydon.emergencyelectricinc.exceptions.ResourceNotFoundException;
+import com.digitalsolutionsbydon.emergencyelectricinc.exceptions.ValidationError;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.ErrorDetail;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
+{
+    @Autowired
+    private MessageSource messageSource;
+
+    @ExceptionHandler({ResourceNotFoundException.class, EntityNotFoundException.class, UsernameNotFoundException.class})
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest req)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Objects in the rear view mirror may appear closer than they are.");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass()
+                                            .getName());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<?> handleNotAuthorizedException(NotAuthorizedException nae, HttpServletRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.UNAUTHORIZED.value());
+        errorDetail.setTitle("YOU SHALL NOT PASS! -- Gandalf The Grey");
+        errorDetail.setDetail(nae.getMessage());
+        errorDetail.setDeveloperMessage(nae.getClass()
+                                           .getName());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> handleBadRequestException(BadRequestException bae, HttpServletRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle("Success depends upon previous preparation, and without such preparation there is sure to be failure. --Confucius");
+        errorDetail.setDetail(bae.getMessage());
+        errorDetail.setDeveloperMessage(bae.getClass()
+                                           .getName());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getMethod());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("HTTP Method Not Valid for Endpoint (check for valid URI and proper HTTP Method)");
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle(ex.getPropertyName());
+        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDeveloperMessage(request.getDescription(true));
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getRequestURL());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("Rest Handler Not Found (check for valid URI)");
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle("Success depends upon previous preparation, and without such preparation there is sure to be failure. --Confucius");
+        errorDetail.setDetail("Input validation failed, check errors Object.");
+        errorDetail.setDeveloperMessage(ex.getClass()
+                                          .getName());
+        List<FieldError> fieldErrors = ex.getBindingResult()
+                                         .getFieldErrors();
+        for (FieldError fe : fieldErrors)
+        {
+            List<ValidationError> validationErrorList = errorDetail.getErrors()
+                                                                   .get(fe.getField());
+            if (validationErrorList == null)
+            {
+                validationErrorList = new ArrayList<>();
+                errorDetail.getErrors()
+                           .put(fe.getField(), validationErrorList);
+            }
+            ValidationError validationError = new ValidationError();
+            validationError.setCode(fe.getCode());
+            validationError.setMessage(fe.getDefaultMessage());
+            validationErrorList.add(validationError);
+        }
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+    }
+}
