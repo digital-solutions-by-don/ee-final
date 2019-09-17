f4409da7ac0c6fab994de175f94c6dafe178eb30
Donald Whitely <dswhitely1@gmail.com>
2019-09-17 12:20:04 -0400

    App.jsx Defined

---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './components/App';
+
+const application = <Provider store={store}><Router><App /></Router></Provider>
+const rootDocument = document.getElementById('root');
+
+render(application, rootDocument);
