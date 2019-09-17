73fc78ba7abd1c637b98c929f292d9d767f0dc0c
Donald Whitely <dswhitely1@gmail.com>
2019-09-17 15:01:59 -0400

    Added Navigation and Footer

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ActionsProvider } from '../contexts/ActionsContext';
 import { useMessageActions } from '../store/message/useMessageActions';
+import Navigation from './shared/navigation/Navigation';
+import Footer from './shared/footer/Footer';
 
 function App () {
   const messageActions = useMessageActions();
   return (
     <ActionsProvider value={{ messageActions }}>
+      <Navigation/>
       <h1>Hello World</h1>
+      <Footer/>
     </ActionsProvider>
   );
 }
