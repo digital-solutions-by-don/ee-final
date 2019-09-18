a8fabdcc422136e32805571417f7de5a5889fabb
Donald Whitely <dswhitely1@gmail.com>
2019-09-18 10:46:36 -0400

    Added Axios Config helper

--- client/src/store/message/useMessageActions.jsx
+++ client/src/store/message/useMessageActions.jsx
@@ -6,13 +6,13 @@ import {
   ADD_MESSAGE_SUCCESS,
   TOGGLE_MESSAGE,
 } from './messageTypes';
-import axios from 'axios';
+import {axiosNoAuth as axios} from '../utils/axiosConfig';
 
 export const useMessageActions = () => {
   const dispatch = useDispatch();
   const addMessage = useCallback(newMessage => {
     dispatch({ type: ADD_MESSAGE_START });
-    axios.post('http://192.168.1.100:5000/api/messages', newMessage)
+    axios().post('/messages', newMessage)
       .then(() => dispatch({ type: ADD_MESSAGE_SUCCESS }))
       .catch(error => dispatch(
         { type: ADD_MESSAGE_FAILURE, payload: error.response }));
