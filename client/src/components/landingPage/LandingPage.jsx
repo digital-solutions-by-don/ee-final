be88fddb3b545bf3c4f201cf5ac6000759718f26
Donald Whitely <dswhitely1@gmail.com>
2019-09-18 07:10:22 -0400

    Added Landing Page

---
+++ client/src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header/Header';
+import About from './about/About';
+import Services from './services/Services';
+
+function LandingPage () {
+  return (
+    <>
+      <Header />
+      <About />
+      <Services />
+    </>
+  );
+}
+
+export default LandingPage;
