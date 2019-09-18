e5e1ead2ec18eb64100c32d753ad1d09c7c0da10
Donald Whitely <dswhitely1@gmail.com>
2019-09-18 09:11:14 -0400

    Added ToggleMessage Action

--- client/src/store/message/useMessageActions.jsx
+++ client/src/store/message/useMessageActions.jsx
@@ -4,6 +4,7 @@ import {
   ADD_MESSAGE_FAILURE,
   ADD_MESSAGE_START,
   ADD_MESSAGE_SUCCESS,
+  TOGGLE_MESSAGE,
 } from './messageTypes';
 import axios from 'axios';
 
@@ -11,11 +12,14 @@ export const useMessageActions = () => {
   const dispatch = useDispatch();
   const addMessage = useCallback(newMessage => {
     dispatch({ type: ADD_MESSAGE_START });
-    axios.post('http://localhost:5000/api/messages', newMessage)
+    axios.post('http://192.168.1.100:5000/api/messages', newMessage)
       .then(() => dispatch({ type: ADD_MESSAGE_SUCCESS }))
       .catch(error => dispatch(
         { type: ADD_MESSAGE_FAILURE, payload: error.response }));
   }, [dispatch]);
 
-  return {addMessage};
+  const toggleMessage = useCallback(() => dispatch({ type: TOGGLE_MESSAGE }),
+    [dispatch]);
+
+  return { addMessage, toggleMessage };
 };
