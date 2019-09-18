0c087ada89408fbe40d15d7884bafbf02800f03f
Donald Whitely <dswhitely1@gmail.com>
2019-09-18 09:11:28 -0400

    Landing Page Complete

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './header/Header';
 import About from './about/About';
 import Services from './services/Services';
+import Testimonials from './testimonials/Testimonials';
+import Goals from './goals/Goals';
+import Contact from './contact/Contact';
+import { useSelector } from 'react-redux';
+import MessageDisplay from './messageDisplay/MessageDisplay';
 
 function LandingPage () {
+  const [showDisplay, setShowDisplay] = useState(false);
+  const isSuccess = useSelector(state => state.messages.isSuccess);
+  const errors = useSelector(state => state.messages.errors);
+
+  useEffect(() => {
+    if (isSuccess || errors) {
+      setShowDisplay(true);
+    } else {
+      setShowDisplay(false);
+    }
+  }, [isSuccess, errors]);
+
   return (
     <>
       <Header />
       <About />
       <Services />
+      <Testimonials />
+      <Goals />
+      <Contact />
+      {showDisplay && <MessageDisplay />}
     </>
   );
 }
