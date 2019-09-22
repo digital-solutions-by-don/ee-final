ee4c345bf0608c2972cd94fdfcc3b8578818d74d
Donald Whitely <dswhitely1@gmail.com>
2019-09-22 18:34:06 -0400

    User Profiles mapping defined.

---
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/models/UserProfiles.java
@@ -0,0 +1,77 @@
+package com.digitalsolutionsbydon.emergencyelectricinc.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class UserProfiles extends Auditable implements Serializable
+{
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    @JsonIgnoreProperties("userProfiles")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="profileId", referencedColumnName = "id")
+    @JsonIgnoreProperties("userProfiles")
+    private Profile profile;
+
+    public UserProfiles()
+    {
+    }
+
+    public UserProfiles(User user, Profile profile)
+    {
+        this.user = user;
+        this.profile = profile;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public Profile getProfile()
+    {
+        return profile;
+    }
+
+    public void setProfile(Profile profile)
+    {
+        this.profile = profile;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (!(o instanceof UserProfiles))
+        {
+            return false;
+        }
+        UserProfiles that = (UserProfiles) o;
+        return getUser().equals(that.getUser()) && getProfile().equals(that.getProfile());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(getUser(), getProfile());
+    }
+}
