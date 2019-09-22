ad90564ecc5a562cd4dd583a46d45b034cdb80cc
Donald Whitely <dswhitely1@gmail.com>
2019-09-22 18:32:45 -0400

    Profile Phone Mapping Defined

---
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/models/ProfilePhone.java
@@ -0,0 +1,77 @@
+package com.digitalsolutionsbydon.emergencyelectricinc.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class ProfilePhone extends Auditable implements Serializable
+{
+    @Id
+    @ManyToOne
+    @JoinColumn(name="profileId", referencedColumnName = "id")
+    @JsonIgnoreProperties("profilePhone")
+    private Profile profile;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="phoneId", referencedColumnName = "id")
+    @JsonIgnoreProperties("profilePhone")
+    private Phone phone;
+
+    public ProfilePhone()
+    {
+    }
+
+    public ProfilePhone(Profile profile, Phone phone)
+    {
+        this.profile = profile;
+        this.phone = phone;
+    }
+
+    public Profile getProfile()
+    {
+        return profile;
+    }
+
+    public void setProfile(Profile profile)
+    {
+        this.profile = profile;
+    }
+
+    public Phone getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(Phone phone)
+    {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (!(o instanceof ProfilePhone))
+        {
+            return false;
+        }
+        ProfilePhone that = (ProfilePhone) o;
+        return getProfile().equals(that.getProfile()) && getPhone().equals(that.getPhone());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(getProfile(), getPhone());
+    }
+}
