2bd98e3a09583e8683c7a559b289ca230f69dd9e
Donald Whitely <dswhitely1@gmail.com>
2019-09-22 14:35:04 -0400

    Private Route, Dashboard imported.  User's actions defined

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,22 @@ import Footer from './shared/footer/Footer';
 import LandingPage from './landingPage/LandingPage';
 import Login from './auth/Login';
 import { useAuthActions } from '../store/auth/useAuthActions';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './dashboard/Dashboard';
+import { useUserActions } from '../store/user/useUserActions';
 
 function App () {
   const messageActions = useMessageActions();
   const authActions = useAuthActions();
+  const userActions = useUserActions();
   return (
-    <ActionsProvider value={{ authActions, messageActions }}>
+    <ActionsProvider value={{ authActions, messageActions, userActions }}>
       <Navigation />
       <Switch>
+        <PrivateRoute
+          path='/dashboard'
+          component={Dashboard}
+        />
         <Route
           path='/register'
           render={props => <Login
