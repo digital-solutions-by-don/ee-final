a390cb09b11f4b8235d78a4c0265ad05aa0a849e
Donald Whitely <dswhitely1@gmail.com>
2019-09-23 08:00:25 -0400

    Updating gitignore files

--- server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/SeedData.java
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/SeedData.java
@@ -1,10 +1,7 @@
 package com.digitalsolutionsbydon.emergencyelectricinc;
 
-import com.digitalsolutionsbydon.emergencyelectricinc.models.Role;
-import com.digitalsolutionsbydon.emergencyelectricinc.models.User;
-import com.digitalsolutionsbydon.emergencyelectricinc.models.UserRoles;
-import com.digitalsolutionsbydon.emergencyelectricinc.services.RoleService;
-import com.digitalsolutionsbydon.emergencyelectricinc.services.UserService;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.*;
+import com.digitalsolutionsbydon.emergencyelectricinc.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -22,6 +19,18 @@ public class SeedData implements CommandLineRunner
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private PhoneService phoneService;
+
+    @Autowired
+    private ProfileService profileService;
 
     @Override
     public void run(String... args) throws Exception
@@ -49,5 +58,31 @@ public class SeedData implements CommandLineRunner
         userService.save(u1);
         userService.save(u2);
         userService.save(u3);
+
+
+        Address a1 = new Address("220 Autumn Ridge Dr NW", "Apt 216", "Corydon", "IN", "47112");
+        a1 = addressService.save(a1);
+        List<ProfileAddress> pa = new ArrayList<>();
+        pa.add(new ProfileAddress(new Profile(), a1));
+        Phone p1 = new Phone("8122256539");
+        Phone p2 = new Phone("8122255602");
+        Phone p3 = new Phone("8125961808");
+        p1 = phoneService.save(p1);
+        p2 = phoneService.save(p2);
+        p3 = phoneService.save(p3);
+        List<ProfilePhone> pp = new ArrayList<>();
+        pp.add(new ProfilePhone(new Profile(), p1));
+        pp.add(new ProfilePhone(new Profile(), p2));
+        pp.add(new ProfilePhone(new Profile(), p3));
+        Email e1 = new Email("dswhitely1@gmail.com");
+        e1 = emailService.save(e1);
+        List<ProfileEmail> pe = new ArrayList<>();
+        pe.add(new ProfileEmail(new Profile(), e1));
+        Profile profile = new Profile("Donald", "Scott", "Whitely", "Don", pa, pp, pe);
+        profile = profileService.saveProfile(profile);
+        List<UserProfiles> userProfile = new ArrayList<>();
+        userProfile.add(new UserProfiles(new User(), profile));
+        User u4 = new User("donald", "123456", users, userProfile);
+        u4 = userService.save(u4);
     }
 }
