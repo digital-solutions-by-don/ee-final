170617e84693ce4fdcc5e116695f5b23121f07a6
Donald Whitely <dswhitely1@gmail.com>
2019-09-23 07:51:14 -0400

    Added Save to ProfileService Implementation

--- server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileServiceImpl.java
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileServiceImpl.java
@@ -1,12 +1,16 @@
 package com.digitalsolutionsbydon.emergencyelectricinc.services;
 
 import com.digitalsolutionsbydon.emergencyelectricinc.models.Profile;
-import com.digitalsolutionsbydon.emergencyelectricinc.repositories.RoleRepository;
-import com.digitalsolutionsbydon.emergencyelectricinc.repositories.UserRepository;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfileAddress;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfileEmail;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfilePhone;
+import com.digitalsolutionsbydon.emergencyelectricinc.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
@@ -14,10 +18,7 @@ import java.util.List;
 public class ProfileServiceImpl implements ProfileService
 {
     @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    private ProfileRepository profileRepository;
 
     @Override
     public List<Profile> getProfilesByUserId(long id)
@@ -31,10 +32,35 @@ public class ProfileServiceImpl implements ProfileService
         return null;
     }
 
+    @Transactional
+    @Modifying
     @Override
     public Profile saveProfile(Profile profile)
     {
-        return null;
+        Profile newProfile = new Profile();
+        newProfile.setFirstName(profile.getFirstName());
+        newProfile.setMiddleName(profile.getMiddleName());
+        newProfile.setLastName(profile.getLastName());
+        newProfile.setPreferredName(profile.getPreferredName());
+        List<ProfileAddress> newAddress = new ArrayList<>();
+        for (ProfileAddress pa : profile.getProfileAddresses())
+        {
+            newAddress.add(new ProfileAddress(newProfile, pa.getAddress()));
+        }
+        newProfile.setProfileAddresses(newAddress);
+        List<ProfilePhone> newPhone = new ArrayList<>();
+        for (ProfilePhone pp : profile.getProfilePhones())
+        {
+            newPhone.add(new ProfilePhone(newProfile, pp.getPhone()));
+        }
+        newProfile.setProfilePhones(newPhone);
+        List<ProfileEmail> newEmail = new ArrayList<>();
+        for (ProfileEmail pe : profile.getProfileEmails())
+        {
+            newEmail.add(new ProfileEmail(newProfile, pe.getEmail()));
+        }
+        newProfile.setProfileEmails(newEmail);
+        return profileRepository.save(newProfile);
     }
 
     @Override
