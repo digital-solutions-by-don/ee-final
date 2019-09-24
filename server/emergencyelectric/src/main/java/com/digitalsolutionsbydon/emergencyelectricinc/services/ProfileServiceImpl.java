9b857e38cdda68c6afdb14397d5cd06fc6a2c386
Donald Whitely <dswhitely1@gmail.com>
2019-09-24 07:29:57 -0400

    Added Update Functionality, removed unused methods

--- server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileServiceImpl.java
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileServiceImpl.java
@@ -1,9 +1,13 @@
 package com.digitalsolutionsbydon.emergencyelectricinc.services;
 
+import com.digitalsolutionsbydon.emergencyelectricinc.exceptions.ResourceNotFoundException;
 import com.digitalsolutionsbydon.emergencyelectricinc.models.Profile;
 import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfileAddress;
 import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfileEmail;
 import com.digitalsolutionsbydon.emergencyelectricinc.models.ProfilePhone;
+import com.digitalsolutionsbydon.emergencyelectricinc.repositories.AddressRepository;
+import com.digitalsolutionsbydon.emergencyelectricinc.repositories.EmailRepository;
+import com.digitalsolutionsbydon.emergencyelectricinc.repositories.PhoneRepository;
 import com.digitalsolutionsbydon.emergencyelectricinc.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
@@ -20,23 +24,21 @@ public class ProfileServiceImpl implements ProfileService
     @Autowired
     private ProfileRepository profileRepository;
 
-    @Override
-    public List<Profile> getProfilesByUserId(long id)
-    {
-        return null;
-    }
+    @Autowired
+    private AddressRepository addressRepository;
 
-    @Override
-    public Profile getProfileByProfileId(long id)
-    {
-        return null;
-    }
+    @Autowired
+    private PhoneRepository phoneRepository;
+
+    @Autowired
+    private EmailRepository emailRepository;
 
     @Transactional
     @Modifying
     @Override
     public Profile saveProfile(Profile profile)
     {
+
         Profile newProfile = new Profile();
         newProfile.setFirstName(profile.getFirstName());
         newProfile.setMiddleName(profile.getMiddleName());
@@ -66,12 +68,58 @@ public class ProfileServiceImpl implements ProfileService
     @Override
     public Profile updateProfileById(Profile profile, long id)
     {
-        return null;
+        Profile updateProfile = profileRepository.findById(id)
+                                                 .orElseThrow(() -> new ResourceNotFoundException("The profile cannot be found"));
+        if (profile.getFirstName() != null)
+        {
+            updateProfile.setFirstName(profile.getFirstName());
+        }
+        if (profile.getMiddleName() != null)
+        {
+            updateProfile.setMiddleName(profile.getMiddleName());
+        }
+        if (profile.getLastName() != null)
+        {
+            updateProfile.setLastName(profile.getLastName());
+        }
+        if (profile.getPreferredName() != null)
+        {
+            updateProfile.setPreferredName(profile.getPreferredName());
+        }
+        if (profile.getProfileAddresses()
+                   .size() > 0)
+        {
+            addressRepository.deleteProfileAddressByProfileId(updateProfile.getId());
+            for (ProfileAddress pa : profile.getProfileAddresses())
+            {
+                addressRepository.insertIntoProfileAddress(updateProfile.getId(), pa.getAddress()
+                                                                                    .getId());
+            }
+            updateProfile.setProfileAddresses(profile.getProfileAddresses());
+        }
+        if (profile.getProfilePhones()
+                   .size() > 0)
+        {
+            phoneRepository.deleteProfilePhoneByProfileId(updateProfile.getId());
+            for (ProfilePhone pp : profile.getProfilePhones())
+            {
+                phoneRepository.insertIntoProfilePhone(updateProfile.getId(), pp.getPhone()
+                                                                                .getId());
+            }
+            updateProfile.setProfilePhones(profile.getProfilePhones());
+        }
+        if (profile.getProfileEmails()
+                   .size() > 0)
+        {
+            emailRepository.deleteProfileEmailByProfileId(updateProfile.getId());
+            for (ProfileEmail pe : profile.getProfileEmails())
+            {
+                emailRepository.insertIntoProfileEmail(updateProfile.getId(), pe.getEmail()
+                                                                                .getId());
+            }
+            updateProfile.setProfileEmails(profile.getProfileEmails());
+        }
+        return profileRepository.save(updateProfile);
     }
 
-    @Override
-    public void deleteProfileById(long id)
-    {
-
-    }
 }
