4810b15510f116d2a94422957639318f89307dd4
Donald Whitely <dswhitely1@gmail.com>
2019-09-24 21:00:44 -0400

    Reformatted ProfileService

--- server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileService.java
+++ server/emergencyelectric/src/main/java/com/digitalsolutionsbydon/emergencyelectricinc/services/ProfileService.java
@@ -1,11 +1,11 @@
 package com.digitalsolutionsbydon.emergencyelectricinc.services;
 
 import com.digitalsolutionsbydon.emergencyelectricinc.models.Profile;
-
-import java.util.List;
+import com.digitalsolutionsbydon.emergencyelectricinc.models.User;
 
 public interface ProfileService
 {
-    Profile saveProfile(Profile profile);
+    Profile saveProfile(Profile profile, User user);
+
     Profile updateProfileById(Profile profile, long id);
 }
